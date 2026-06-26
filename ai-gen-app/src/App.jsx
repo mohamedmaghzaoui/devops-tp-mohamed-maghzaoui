e6ca6210ae671aef15d2dd4437e2ffa4e19e0a57
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2026-06-26 12:12:49 +0200

    release(front): version 1.0.0

--- ai-gen-app/src/App.jsx
+++ ai-gen-app/src/App.jsx
@@ -1,14 +1,21 @@
+/* global __APP_VERSION__ */
 import aigenImage from './assets/Try our AiGen.png'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { Form } from './components/form'
 import { Data } from './components/data'
 
+const version = __APP_VERSION__
+
 function App() {
   return (
     <div className="container-fluid min-vh-100 d-flex flex-column align-items-center">
+      <div className="w-100 d-flex justify-content-between align-items-center mt-3 px-3">
+        <span className="text-light small">Version {version}</span>
+      </div>
+
       {/* HEADER */}
-      <div className="text-center mt-5">
+      <div className="text-center mt-2">
         <img
           className="img-fluid mb-3"
           style={{ maxWidth: '220px' }}
